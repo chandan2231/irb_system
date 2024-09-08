19e37fe3f05da09b3f821a7eea3c80ec36d8f4eb
am1t-kumar <thorragnarak@gmail.com>
2024-09-08 10:24:01 +0530

    ref: code ref

--- api/templates/constant.js
+++ api/templates/constant.js
@@ -7,6 +7,8 @@ const continuingReviewQuestions = {
                 medical monitor, data safety monitoring board (DSMB), or a data
                 monitoring committee (DMC) provided any correspondence that has
                 not yet been reported to the IRB?`,
+      answer: "irb_report",
+      explanation: "irb_report_explain",
     },
     question2: {
       text: `Since the date of the last approval, have you encountered any
@@ -26,22 +28,31 @@ const continuingReviewQuestions = {
                   economic, or social harm) related to the research than was
                   previously known or recognized.`,
       },
+      answer: "criteria_report",
+      explanation: "criteria_report_explain",
     },
   },
   informedConsentProcess: {
     title: `Informed Consent Process`,
     question1: {
       text: `Which version of the ICF are you currently using?`,
+      answer: "icf_version",
+      pdfLink: "",
     },
     question2: {
       text: `Who is performing the informed consent at your site?`,
+      answer: "performing_consent",
     },
     question3: {
       text: `Have there been any challenges faced to the consenting process?`,
+      answer: "challenges_faced",
+      explanation: "challenges_faced_explain",
     },
     question4: {
       text: `Have there been any changes to the consent form that have not been
                 reported to the IRB?`,
+      answer: "changes_consent",
+      explanation: "changes_consent_explain",
     },
     question5: {
       text: `Are you ensuring that:`,
@@ -58,6 +69,9 @@ const continuingReviewQuestions = {
         option5: `5. All participants have been consented or re-consented, where
                   necessary, with the most current approved informed consent form?`,
       },
+      answer: "ensuring_list",
+      explanation: "ensuring_list_explain",
+      pdfLink: "",
     },
   },
   investigatorInstuationInfo: {
@@ -65,41 +79,54 @@ const continuingReviewQuestions = {
     question1: {
       text: `Have there been any changes in the investigator’s situation or
                 qualifications?`,
+      answer: "inv_sit_quali",
     },
     question2: {
       text: `Have there been any investigation of or complaints related to the
                 investigator’s conduct of research?`,
+      answer: "inv_or_comp",
+      explanation: "inv_or_comp_explain",
     },
     question3: {
       text: `Have there been any changes in the facility’s ability to
                 adequately support the research protocol?`,
+      answer: "facility_changes",
     },
     question4: {
       text: `Have there been any changes in facility regulations, standard
                 operating procedures, or standards of professional conduct?`,
+      answer: "facility_any_changes",
+      explanation: "facility_any_changes_explain",
     },
     question5: {
       text: `Have there been any changes to state or local law regarding
                 research that affects the conduct of research?`,
+      answer: "changes_law",
+      explanation: "changes_law_explain",
     },
   },
   researchProgressInfo: {
     title: `Research Progress`,
     question1: {
       text: `Total Subjects Enrolled`,
+      answer: "subjects_enrolled",
     },
     question2: {
       text: `How many subjects have discontinued their participation?`,
+      answer: "discontinued_subjects",
     },
     question3: {
       text: `How many adverse events have occurred since the last approval?`,
+      answer: "occured_adverse_event",
     },
     question4: {
       text: `How many subject have completed the study per protocol?`,
+      answer: "subjecte_completed",
     },
     question5: {
       text: `Have there been any updates/changes to the protocol since the last
                 approval?`,
+      answer: "last_approval_change",
     },
   },
 };
--- api/templates/html-templates.js
+++ api/templates/html-templates.js
@@ -5,27 +5,23 @@ const renderHeader = (props) => {
   return `<h3>${title}</h3>`;
 };
 
-const RenderQuestion = (sequence, question) => {
+const RenderQuestionAndAnswer = (sequence, question, answerObject) => {
+  const { text, subOptions, answer, explanation, pdfLink } = question;
   return `<div>
-      ${sequence && `<h4>Question ${sequence}</h4>`}
-      ${question.text ? `<span>${question.text}</span>` : ``}
+      <h4>Question ${sequence}</h4>
+      ${text ? `<span>${text}</span>` : ``}
       ${
-        question.subOptions
+        subOptions
           ? `<ul>
-              ${Object.keys(question.subOptions)
+              ${Object.keys(subOptions)
                 .map((key) => {
-                  return `<li>${question.subOptions[key]}</li>`;
+                  return `<li>${subOptions[key]}</li>`;
                 })
                 .join("")}
             </ul>`
           : ""
       }
-    </div>`;
-};
-
-const RenderAnswer = (answerObject, mappedKeys) => {
-  const { answer, explanation = "", pdfLink = "" } = mappedKeys;
-  return `<div>
+      <div>
       <h4>Answer</h4>
       ${
         answerObject[answer]
@@ -38,7 +34,8 @@ const RenderAnswer = (answerObject, mappedKeys) => {
              <span>${answerObject[explanation]}</span>`
           : ``
       }
-    </div>`;
+    </div>
+  </div>`;
 };
 
 const continuingReviewHTMLTemplate = (templateProps) => {
@@ -63,128 +60,135 @@ const continuingReviewHTMLTemplate = (templateProps) => {
         <div style="page-break-after: always;">
           <div>${renderHeader(riskAssessment)}</div>  
           <div>
-            ${RenderQuestion(1, riskAssessment.question1)}
-            ${RenderAnswer(risk_assessment, {
-              answer: "irb_report",
-              explanation: "irb_report_explain",
-            })}
+            ${RenderQuestionAndAnswer(
+              1,
+              riskAssessment.question1,
+              risk_assessment
+            )}
           </div>
           <div>
-            ${RenderQuestion(2, riskAssessment.question2)}
-             ${RenderAnswer(risk_assessment, {
-               answer: "criteria_report",
-               explanation: "criteria_report_explain",
-             })}
+            ${RenderQuestionAndAnswer(
+              2,
+              riskAssessment.question2,
+              risk_assessment
+            )}
           </div>
         </div>
 
         <div style="page-break-after: always;">
            <div>${renderHeader(informedConsentProcess)}</div>
           <div>
-            ${RenderQuestion(1, informedConsentProcess.question1)}
-            ${RenderAnswer(informed_consent_process, {
-              answer: "icf_version",
-              pdfLink: "",
-            })}
+            ${RenderQuestionAndAnswer(
+              1,
+              informedConsentProcess.question1,
+              informed_consent_process
+            )}
           </div>
           <div>
-            ${RenderQuestion(2, informedConsentProcess.question2)}
-            ${RenderAnswer(informed_consent_process, {
-              answer: "performing_consent",
-            })}
+            ${RenderQuestionAndAnswer(
+              2,
+              informedConsentProcess.question2,
+              informed_consent_process
+            )}
           </div>
           <div>
-            ${RenderQuestion(3, informedConsentProcess.question3)}
-            ${RenderAnswer(informed_consent_process, {
-              answer: "challenges_faced",
-              explanation: "challenges_faced_explain",
-            })}
+            ${RenderQuestionAndAnswer(
+              3,
+              informedConsentProcess.question3,
+              informed_consent_process
+            )}
           </div>
           <div>
-            ${RenderQuestion(4, informedConsentProcess.question4)}
-            ${RenderAnswer(informed_consent_process, {
-              answer: "changes_consent",
-              explanation: "changes_consent_explain",
-            })}
+            ${RenderQuestionAndAnswer(
+              4,
+              informedConsentProcess.question4,
+              informed_consent_process
+            )}
           </div>
           <div>
-            ${RenderQuestion(5, informedConsentProcess.question5)}
-            ${RenderAnswer(informed_consent_process, {
-              answer: "ensuring_list",
-              explanation: "ensuring_list_explain",
-              pdfLink: "",
-            })}
+            ${RenderQuestionAndAnswer(
+              5,
+              informedConsentProcess.question5,
+              informed_consent_process
+            )}
           </div>
         </div>
 
         <div style="page-break-after: always;">
           <div>${renderHeader(investigatorInstuationInfo)}</div>
           <div>
-            ${RenderQuestion(1, investigatorInstuationInfo.question1)}
-            ${RenderAnswer(investigator_instuation_info, {
-              answer: "inv_sit_quali",
-            })}
+            ${RenderQuestionAndAnswer(
+              1,
+              investigatorInstuationInfo.question1,
+              investigator_instuation_info
+            )}
           </div>
           <div>
-            ${RenderQuestion(2, investigatorInstuationInfo.question2)}
-            ${RenderAnswer(investigator_instuation_info, {
-              answer: "inv_or_comp",
-              explanation: "inv_or_comp_explain",
-            })}
+            ${RenderQuestionAndAnswer(
+              2,
+              investigatorInstuationInfo.question2,
+              investigator_instuation_info
+            )}
           </div>
           <div>
-            ${RenderQuestion(3, investigatorInstuationInfo.question3)}
-            ${RenderAnswer(investigator_instuation_info, {
-              answer: "facility_changes",
-            })}
+            ${RenderQuestionAndAnswer(
+              3,
+              investigatorInstuationInfo.question3,
+              investigator_instuation_info
+            )}
           </div>
           <div>
-            ${RenderQuestion(4, investigatorInstuationInfo.question4)}
-            ${RenderAnswer(investigator_instuation_info, {
-              answer: "facility_any_changes",
-              explanation: "facility_any_changes_explain",
-            })}
+            ${RenderQuestionAndAnswer(
+              4,
+              investigatorInstuationInfo.question4,
+              investigator_instuation_info
+            )}
           </div>
           <div>
-            ${RenderQuestion(5, investigatorInstuationInfo.question5)}
-            ${RenderAnswer(investigator_instuation_info, {
-              answer: "changes_law",
-              explanation: "changes_law_explain",
-            })}
+            ${RenderQuestionAndAnswer(
+              5,
+              investigatorInstuationInfo.question5,
+              investigator_instuation_info
+            )}
           </div>
         </div>
 
         <div style="page-break-after: always;">
           <div>${renderHeader(researchProgressInfo)}</div>
           <div>
-            ${RenderQuestion(1, researchProgressInfo.question1)}
-            ${RenderAnswer(research_progress_info, {
-              answer: "subjects_enrolled",
-            })}
+            ${RenderQuestionAndAnswer(
+              1,
+              researchProgressInfo.question1,
+              research_progress_info
+            )}
           </div>
           <div>
-            ${RenderQuestion(2, researchProgressInfo.question2)}
-            ${RenderAnswer(research_progress_info, {
-              answer: "discontinued_subjects",
-            })}
+            ${RenderQuestionAndAnswer(
+              2,
+              researchProgressInfo.question2,
+              research_progress_info
+            )}
           </div>
           <div>
-            ${RenderQuestion(3, researchProgressInfo.question3)}
-            ${RenderAnswer(research_progress_info, {
-              answer: "occured_adverse_event",
-            })}
+            ${RenderQuestionAndAnswer(
+              3,
+              researchProgressInfo.question3,
+              research_progress_info
+            )}
           </div>
           <div>
-            ${RenderQuestion(4, researchProgressInfo.question4)}
-            ${RenderAnswer(research_progress_info, {
-              answer: "subjecte_completed",
-            })}
+            ${RenderQuestionAndAnswer(
+              4,
+              researchProgressInfo.question4,
+              research_progress_info
+            )}
           </div>
           <div>
-            ${RenderQuestion(5, researchProgressInfo.question5)}
-            ${RenderAnswer(research_progress_info, {
-              answer: "last_approval_change",
-            })}
+            ${RenderQuestionAndAnswer(
+              5,
+              researchProgressInfo.question5,
+              research_progress_info
+            )}
           </div>
         </div>
       </main>
