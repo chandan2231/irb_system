97ceb592f7a905e690c026180a2427958bb149c8
Chandan <chandan@Chandans-MacBook-Air.local>
2024-12-17 10:23:42 +0530

    fixes

--- api/index.js
+++ api/index.js
@@ -10,13 +10,31 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app = express()
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true)
-  next()
-})
+
 // middlewares
 app.use(express.json())
-app.use(cors({ origin: 'https://irbhub.com' }))
+
+// Define allowed origins for production
+const allowedOrigins = ['https://irbhub.com'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the origin is in the allowedOrigins list
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,  // Allow credentials (cookies, HTTP authentication, etc.)
+  preflightContinue: false, // Don't pass preflight request to the next handler
+};
+
+// Use the cors middleware
+app.use(cors(corsOptions));
+
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
@@ -26,6 +44,6 @@ app.use('/api/continuinReview', continuinReviewRoutes)
 app.use('/api/eventAndRequest', eventAndRequest)
 app.use('/api/admin', adminRoutes)
 
-app.listen(8800, () => {
+app.listen(8000, () => {
   console.log('API Working!')
 })
