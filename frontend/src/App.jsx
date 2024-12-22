5612d238e780d185f497122bdfc3343f3c618b47
Chandan <chandan@Chandans-MacBook-Air.local>
2024-12-22 22:32:45 +0530

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,7 +47,7 @@ import { Box, CssBaseline } from "@mui/material";
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 function App() {
-  const [loader, setLoader] = useState(true);
+  const [loader, setLoader] = useState(false);
   const dispatch = useDispatch();
   //const navigate = useNavigate();
   function getCookieValue(name) {
@@ -86,33 +86,33 @@ function App() {
   //   }
   // };
 
-  useEffect(() => {
-    const cookieName = "accessToken";
-    const getCookie = getCookieValue(cookieName);
-    let data = { token: getCookie };
-    console.log("data", data);
-    dispatch(validateUserToken(data)).then((data) => {
-      try {
-        console.log("Validate token", data);
-        if (data.payload.status === 200) {
-          setLoader(false);
-          if (["/signin", "/signup", "/"].includes(window.location.pathname)) {
-            window.location.replace("/dashboard");
-          }
-        } else {
-          if (!["/signin", "/signup"].includes(window.location.pathname)) {
-            window.location.replace("/signin");
-          }
-          setLoader(false);
-        }
-      } catch (err) {
-        setLoader(false);
-        if (!["/signin", "/signup"].includes(window.location.pathname)) {
-          window.location.replace("/signin");
-        }
-      }
-    });
-  }, [dispatch]);
+  // useEffect(() => {
+  //   const cookieName = "accessToken";
+  //   const getCookie = getCookieValue(cookieName);
+  //   let data = { token: getCookie };
+  //   console.log("data", data);
+  //   dispatch(validateUserToken(data)).then((data) => {
+  //     try {
+  //       console.log("Validate token", data);
+  //       if (data.payload.status === 200) {
+  //         setLoader(false);
+  //         if (["/signin", "/signup", "/"].includes(window.location.pathname)) {
+  //           window.location.replace("/dashboard");
+  //         }
+  //       } else {
+  //         if (!["/signin", "/signup"].includes(window.location.pathname)) {
+  //           window.location.replace("/signin");
+  //         }
+  //         setLoader(false);
+  //       }
+  //     } catch (err) {
+  //       setLoader(false);
+  //       if (!["/signin", "/signup"].includes(window.location.pathname)) {
+  //         window.location.replace("/signin");
+  //       }
+  //     }
+  //   });
+  // }, [dispatch]);
 
   // useEffect(() => {
   //   validateToken();
