874babdbbcd389ecfea54b882a16de2080d4f69b
Chandan <chandan@Chandans-MacBook-Air.local>
2024-12-26 10:02:53 +0530

    change validate token call

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -57,34 +57,34 @@ function App() {
       return match[2];
     }
   }
-  // const validateToken = async (e) => {
-  //   try {
-  //     const cookieName = "accessToken";
-  //     const getCookie = getCookieValue(cookieName);
-  //     let res = await axios.get(baseURL + "/auth/", {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         token: getCookie,
-  //       },
-  //     });
-  //     if (res.status === 200) {
-  //       setLoader(false);
-  //       if (["/signin", "/signup", "/"].includes(window.location.pathname)) {
-  //         window.location.replace("/dashboard");
-  //       }
-  //     } else {
-  //       if (!["/signin", "/signup"].includes(window.location.pathname)) {
-  //         window.location.replace("/signin");
-  //       }
-  //       setLoader(false);
-  //     }
-  //   } catch (err) {
-  //     setLoader(false);
-  //     if (!["/signin", "/signup"].includes(window.location.pathname)) {
-  //       window.location.replace("/signin");
-  //     }
-  //   }
-  // };
+  const validateToken = async (e) => {
+    try {
+      const cookieName = "accessToken";
+      const getCookie = getCookieValue(cookieName);
+      let res = await axios.get(baseURL + "/auth/", {
+        headers: {
+          "Content-Type": "application/json",
+          token: getCookie,
+        },
+      });
+      if (res.status === 200) {
+        setLoader(false);
+        if (["/signin", "/signup", "/"].includes(window.location.pathname)) {
+          window.location.replace("/dashboard");
+        }
+      } else {
+        if (!["/signin", "/signup"].includes(window.location.pathname)) {
+          window.location.replace("/signin");
+        }
+        setLoader(false);
+      }
+    } catch (err) {
+      setLoader(false);
+      if (!["/signin", "/signup"].includes(window.location.pathname)) {
+        window.location.replace("/signin");
+      }
+    }
+  };
 
   // useEffect(() => {
   //   const cookieName = "accessToken";
@@ -114,9 +114,9 @@ function App() {
   //   });
   // }, [dispatch]);
 
-  // useEffect(() => {
-  //   validateToken();
-  // });
+  useEffect(() => {
+    validateToken();
+  }, []);
   if (loader) {
     return (
       <Backdrop
