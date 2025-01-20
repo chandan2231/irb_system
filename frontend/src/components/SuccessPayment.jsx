9dd3a05bd14ab1c5f94ad8445b1e405424d9b16c
am1t-kumar <thorragnarak@gmail.com>
2025-01-20 22:35:02 +0530

    add: download pdf on payment success

--- frontend/src/components/SuccessPayment.jsx
+++ frontend/src/components/SuccessPayment.jsx
@@ -1,17 +1,41 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Box, Typography, useTheme } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import { useNavigate, useLocation } from "react-router-dom";
+import Loader from "./Loader";
+import { Box, Button, Typography, useTheme } from "@mui/material";
+import { useLocation } from "react-router-dom";
+import { protocolReport } from "../services/UserManagement/UserService";
 
 function SuccessPayment() {
   const theme = useTheme();
+  const [loader, setLoader] = React.useState(false);
   const location = useLocation();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
   const protocolTypeDetails = location.state.details;
   const amount = location.state.amount;
+
+  const handleViewPdf = async () => {
+    const {
+      protocol_id: protocolId,
+      protocol_type: researchType,
+    } = protocolTypeDetails;
+    const protocolReportPayload = {
+      protocolId: protocolId,
+      protocolType: researchType,
+    };
+    try {
+      setLoader(true);
+      let pdfResponse = await protocolReport(protocolReportPayload);
+      setLoader(false);
+      if (pdfResponse !== "") {
+        window.open(pdfResponse.pdfUrl, "_blank", "noopener,noreferrer");
+      }
+    } catch (error) {
+      setLoader(false);
+      console.log(error);
+    }
+  };
+
+  if (loader) {
+    return <Loader />;
+  }
 
   return (
     <>
@@ -30,6 +54,13 @@ function SuccessPayment() {
           <Typography variant="h6" mb={2}>
             Protocol Type: {protocolTypeDetails?.protocol_type}
           </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => handleViewPdf()}
+          >
+            Download Protocol
+          </Button>
         </Box>
       </Box>
     </>
