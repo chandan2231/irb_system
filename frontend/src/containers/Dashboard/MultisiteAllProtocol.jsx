f349367dbfd75ae746c478e4c7b166b779f8df88
am1t-kumar <thorragnarak@gmail.com>
2025-01-22 20:26:33 +0530

    add: changes

--- frontend/src/containers/Dashboard/MultisiteAllProtocol.jsx
+++ frontend/src/containers/Dashboard/MultisiteAllProtocol.jsx
@@ -1,43 +1,51 @@
 import React, { useEffect } from "react";
 import CommonModal from "../../components/CommonModal/Modal";
+import Loader from "../../components/Loader";
 import { getMultiSiteChildProtocols } from "../../services/Dashboard/DashboardService";
 import { useDispatch, useSelector } from "react-redux";
 
 const MultisiteChildProtocol = ({ open, onClose, data = {} }) => {
-    const [tableDetails, setTableDetails] = React.useState([]);
+    const [loader, setLoader] = React.useState(false);
     const dispatch = useDispatch();
 
-    // const { protocolList, loading, error, createdProtocol } = useSelector(
-    //     (state) => ({
-    //         error: state.dashboard.error,
-    //         protocolList: state.dashboard.protocolList,
-    //         loading: state.dashboard.loading,
-    //         createdProtocol: state.dashboard.createdProtocol,
-    //     })
-    // );
-
-    const getContentForHaveProtocolId = () => {
-        return <>hiiii</>;
-    };
+    const { multiSiteChildProtocolsList, loading, error } = useSelector(
+        (state) => ({
+            error: state.dashboard.error,
+            multiSiteChildProtocolsList: state.dashboard.multiSiteChildProtocolsList,
+            loading: state.dashboard.loading,
+        })
+    );
 
     useEffect(() => {
         const getDetails = async () => {
             console.log(data);
             if (data?.protocolId) {
-                return dispatch(getMultiSiteChildProtocols(data?.protocolId));
+                setLoader(true);
+                dispatch(getMultiSiteChildProtocols(data?.protocolId)).then((data) => {
+                    setLoader(false);
+                });
             }
         }
         getDetails();
     }, [dispatch, data])
 
+    console.group("multiSiteChildProtocolsList ====>", multiSiteChildProtocolsList)
+
+    const getContentForHaveProtocolId = () => {
+        return <>hiiii</>;
+    };
+
+    if (loader) {
+        return <Loader />;
+    }
+
     return (
         <React.Fragment>
             <CommonModal
                 open={open}
-                onClose={() => {
-                    setTableDetails([]);
-                    onClose();
-                }}
+                onClose={() =>
+                    onClose()
+                }
                 title="Multisite Child Protocol"
                 subTitle=""
                 content={getContentForHaveProtocolId()}
