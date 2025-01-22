51059a37d03c1344b4d7b3c3fb88711df1b2b334
am1t-kumar <thorragnarak@gmail.com>
2025-01-22 19:05:29 +0530

    fix: message

--- frontend/src/containers/Dashboard/Dashboard.jsx
+++ frontend/src/containers/Dashboard/Dashboard.jsx
@@ -228,6 +228,18 @@ function Dashboard() {
             progress: undefined,
             theme: "dark",
           });
+        } else {
+          setOpen(false);
+          toast.error(data.payload.data.msg, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
         }
       });
     }
